ebc557cebc121ba005475c32dcd28554ba554448
ahmed-elsabbagh778 <ahmedelsabbagh973@gmail.com>
2025-05-04 12:49:21 +0300

    updated axios config

--- src/apis/config.js
+++ src/apis/config.js
@@ -4,29 +4,3 @@ export const axiosInstance = axios.create({
   baseURL: "https://dummyjson.com",
 });
 
-// Add a request interceptor
-axiosInstance.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
-    config.headers['Authorization'] = 'ACCESS_TOKEN_FROM_INTERCEPTORS'
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
-
-// Add a response interceptor
-axiosInstance.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
